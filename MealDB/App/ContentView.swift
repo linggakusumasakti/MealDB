fa66a126c0d76b9c6fcd30d65d230fc8f1aabd36
linggakusumasakti <linggakusumasakti@gmail.com>
2021-12-13 11:26:20 +0700

    change to floating tab bar

--- MealDB/App/ContentView.swift
+++ MealDB/App/ContentView.swift
@@ -10,21 +10,87 @@ import SwiftUI
 struct ContentView: View {
     @EnvironmentObject var homePresenter: HomePresenter
     @EnvironmentObject var searchPresenter: SearchPresenter
+    @State var selected = 0
+    
     var body: some View {
         NavigationView {
-            TabView {
-                HomeView(presenter: homePresenter).tabItem {
-                    TabBarIcon(imageName: "house", title: "Home")
+            ZStack {
+                VStack {
+                    if self.selected == 0 {
+                        
+                        HomeView(presenter: homePresenter)
+                        
+                    } else {
+                        
+                        SearchView(presenter: searchPresenter)
+                        
+                    }
                 }
                 
-                SearchView(presenter: searchPresenter).tabItem {
-                    TabBarIcon(imageName: "magnifyingglass", title: "Search")
-                }
+                FloatingTabBar(selected: self.$selected)
                 
-              
-                
-            }.accentColor(.red)
+            }
+            
         }.navigationViewStyle(StackNavigationViewStyle())
+        //        NavigationView {
+        //            TabView {
+        //                HomeView(presenter: homePresenter).tabItem {
+        //                    TabBarIcon(imageName: "house", title: "Home")
+        //                }
+        //
+        //                SearchView(presenter: searchPresenter).tabItem {
+        //                    TabBarIcon(imageName: "magnifyingglass", title: "Search")
+        //                }
+        //
+        //
+        //
+        //            }.accentColor(.red)
+        //        }.navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct FloatingTabBar: View {
+    
+    @Binding var selected: Int
+    
+    var body: some View {
+        HStack {
+            Spacer(minLength: 0)
+            
+            HStack {
+                
+                Spacer()
+                
+                Button(action: {
+                    self.selected = 0
+                }) {
+                    Image(systemName: "house")
+                        .resizable()
+                        .frame(width: 25, height: 25, alignment: .center)
+                        .foregroundColor(self.selected == 0 ? .red : .gray)
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    self.selected = 1
+                }) {
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .frame(width: 25, height: 25, alignment: .center)
+                        .foregroundColor(self.selected == 1 ? .red : .gray)
+                }
+                
+                Spacer()
+                
+            }
+            
+        }.padding(16)
+            .background(Color.white)
+            .clipShape(Capsule())
+            .shadow(color: .gray, radius: 8)
+            .padding(22)
+            .frame(maxHeight: .infinity, alignment: .bottom)
     }
 }
 
